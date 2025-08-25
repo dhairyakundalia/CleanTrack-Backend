470fe5951a25164d2a7942705971c896899d3ddb
kacha parth <kachaparth123@gmail.com>
2025-08-25 18:57:13 +0530

    update in socket

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,6 +1,8 @@
 import * as UserModel from "../models/user.model.js";
 
 const userSocketHandler = (socket) => {
+
+
     socket.on("getLiveUpdates", ({user_id}) => {
         UserModel.joinGeofences({socket, user_id})
     });
